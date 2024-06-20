8c74685fe0dc8a2ee4255545dd6ddada1cba966d
Lautaro Bertarelli <lautaber9@gmail.com>
2024-06-20 17:58:32 -0300

    extras

--- SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
+++ SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d78c73ac2e912cbf2f1f7708cb461ddc3453579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a69b453e516af4f4b5989202608efa6e82f82e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
